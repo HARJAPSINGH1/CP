216c110e633f55ad6f3e0e6286a22f58895058e3
HARJAPSINGH1 <harjapsingh12806@gmail.com>
2026-05-29 13:15:35 +0530

    Added 4A and .gitignore

--- rating_800/A71_WayTooLongWords.java
+++ rating_800/A71_WayTooLongWords.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 public class A71_WayTooLongWords {
-    
-    public static void main(String args[]){
-        
+
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        
+
         int n = sc.nextInt();
-        
-        for(int i = 0; i < n; i++){
-            
+
+        for (int i = 0; i < n; i++) {
+
             String word = sc.next();
             int m = word.length();
-            
-            if(m > 10){
+
+            if (m > 10) {
                 String str = "" + word.charAt(0) + (m - 2) + word.charAt(m - 1);
                 System.out.println(str);
-            }else {
+            } else {
                 System.out.println(word);
             }
         }
