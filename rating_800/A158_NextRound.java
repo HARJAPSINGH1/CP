a6a22000978934d56b3534a1623c027b01107f1c
HARJAPSINGH1 <harjapsingh12806@gmail.com>
2026-05-29 15:03:06 +0530

    Added 158A, 50A, 282A

---
+++ rating_800/A158_NextRound.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class A158_NextRound {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int list[] = new int[n];
+
+        for(int i = 0; i < n; i++){
+
+            list[i] = sc.nextInt();
+        }
+
+        int kth = list[k - 1];
+
+        int count = 0;
+
+        for(int i = 0; i < n; i++){
+            
+            if(list[i] >= kth && list[i] > 0){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+    
+}
---
+++ rating_800/A282_Bit.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class A282_Bit {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x = 0;
+
+        for(int i = 0; i < n; i++){
+
+            String operation = sc.next();
+
+            if(operation.charAt(1) == '+'){
+                x++;
+            }else{
+                x--;
+            }
+        }
+
+        System.out.println(x);
+        
+    }
+    
+}
